b5b2fc086b91b8ecbf71194dc284e72a5752119b
Violetta2413 <noszek.violetta2004@gmail.com>
2020-11-06 15:54:47 +0000

    Initial commit

---
+++ f1.py
@@ -0,0 +1,8 @@
+print ('Hello World! ') #ez egy megjegyzés
+#ez egy megjegyzés 
+print ('Hello') 
+''' első sor
+második sor
+harmadik sor
+'''
+print('itt a vége')
---
+++ f2.py
@@ -0,0 +1,6 @@
+print('Hello ....!')
+print('Nagyon jó ez a program!')
+print('Üdv, a program.')
+
+print()
+print("Hello ....! \nNagyon jó ez a program!\nÜdv, a program.")
